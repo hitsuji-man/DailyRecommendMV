4c7ac3e352b6016baf018961b5a5c8430b5ad575
hitsuji-man <renton1q84@gmail.com>
2026-01-10 22:42:02 +0900

    匿名即時ログイン用にUserモデルを修正

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,6 +45,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAnonymous():bool
+    {
+        return is_null($this->email);
+    }
+
+    public function isVerified():bool
+    {
+        return !is_null($this->email);
     }
 
     public function userHistories() {
