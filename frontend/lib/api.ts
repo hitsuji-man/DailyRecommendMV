02967d86aae9ef3c3bf222619e3462011e4b7d78
hitsuji-man <renton1q84@gmail.com>
2026-01-27 17:17:14 +0900

    Next.jsから呼ぶAPIの基準URLの設定(.envも)

---
+++ frontend/lib/api.ts
@@ -0,0 +1,8 @@
+// lib/api.ts
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+if (!baseUrl) {
+  throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
+}
+
+export const API_BASE_URL = baseUrl;
